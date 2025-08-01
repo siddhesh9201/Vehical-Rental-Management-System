e9b9d3d55a4f9526d6e867d99041efb210f4cdaf
siddhesh9201 <siddheshmali3050@gmail.com>
2025-08-01 21:00:12 +0530

    Initial commit

---
+++ src/main/java/com/VehicalRentalManagementSystem/VehicalRentalManagementSystem/VehicalRentalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.VehicalRentalManagementSystem.VehicalRentalManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicalRentalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicalRentalManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VehicalRentalManagementSystem/VehicalRentalManagementSystem/VehicalRentalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VehicalRentalManagementSystem.VehicalRentalManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicalRentalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
